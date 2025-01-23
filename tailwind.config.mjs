d2b19981ba9ba5693be7e10e20d20242976d5530
MethuParoi <paroimethu@gmail.com>
2025-01-23 20:45:36 +0530

    navbar and footer added

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,17 +1,25 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+const config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+  plugins: [require("daisyui")],
+  daisyui: {
+    theme: [
+      {
+        colors: {
+          primary: "#D8EFD3",
+          secondary: "#55AD9B",
+          accent: "#95D2B3",
+          neutral: "#F1F8E8",
+          "base-100": "#ffffff",
+          "base-200": "#EEEEEE",
+        },
       },
-    },
+    ],
   },
-  plugins: [],
 };
+
+export default config;
