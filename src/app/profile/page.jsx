e9015a9370c8f2b6b80d018202fa90ec97a491ae
MethuParoi <paroimethu@gmail.com>
2025-01-24 11:51:15 +0530

    minor issue fixed

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,13 +1,24 @@
 import React from "react";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
 
-const page = () => {
-  return (
-    <div className="flex items-center justify-center">
-      <h1 className="text-3xl font-semibold text-primary ">
-        Welcome to your profile!
-      </h1>
-    </div>
-  );
+const page = async () => {
+  const { isAuthenticated } = getKindeServerSession();
+  const isUserAuthenticated = await isAuthenticated();
+
+  if (!isUserAuthenticated) {
+    redirect("/");
+  }
+
+  if (isUserAuthenticated) {
+    return (
+      <div className="flex items-center justify-center">
+        <h1 className="text-3xl font-semibold text-primary ">
+          Welcome to your profile!
+        </h1>
+      </div>
+    );
+  }
 };
 
 export default page;
