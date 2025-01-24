a44d787fe895e73cb3b88733ab539081aecaf899
MethuParoi <paroimethu@gmail.com>
2025-01-24 10:36:40 +0530

    bug fixes in login route

--- src/app/api/auth/[kindeAuth]/route.js
+++ src/app/api/auth/[kindeAuth]/route.js
@@ -1,2 +1,13 @@
+// import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
+// export const GET = handleAuth();
+
 import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
-export const GET = handleAuth();
+
+const kindeConfig = {
+  clientId: process.env.KINDE_CLIENT_ID,
+  clientSecret: process.env.KINDE_CLIENT_SECRET,
+  redirectUri: process.env.KINDE_REDIRECT_URI,
+  // Add other necessary configurations
+};
+
+export const GET = handleAuth(kindeConfig);
